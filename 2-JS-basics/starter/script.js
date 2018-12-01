53b8d46af683bc83c75c39501322b2d6f2081818
Joanna <jmgsese@gmail.com>
2018-12-01 12:58:59 -0600

    Messing around

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(firstName + ' is ' + age);
+console.log(`${firstName} ${age}`)
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+alert(`${firstName} is a bum!`)
