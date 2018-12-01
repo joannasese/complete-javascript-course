bdaab059d1d2385edb5b52e05aa9544f4f6af55f
Joanna <jmgsese@gmail.com>
2018-12-01 13:48:06 -0600

    Complete first code challenge

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,17 +1,35 @@
-var firstName = 'John';
-console.log(firstName);
+/*
+* CODING CHALLENGE 1
+*/
 
-var lastName = 'Smith';
-var age = 28;
+/*
+Mark and John are trying to compare their BMI
+(Body Mass Index), which is calculated using the formula:
+BMI = mass / height^2 = mass / (height * height).
+(mass in kg and height in meter).
 
-var fullAge = true;
-console.log(firstName + ' is ' + age);
-console.log(`${firstName} ${age}`)
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about
+whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable
+from step 3. (Something like "Is Mark's BMI higher than
+John's? true").
 
-var job, isMarried;
-job = 'teacher';
-isMarried = false;
+GOOD LUCK 😀
+*/
 
-// alert(`${firstName} is a bum!`);
-var bum = prompt(`${firstName} is a bum, don't you think?`);
-console.log(bum);
+var markMass, markHeight, johnMass, johnHeight;
+markMass = 72;
+markHeight = 2;
+johnMass = 68;
+johnHeight = 2.5;
+
+var markBMI = markMass / Math.pow(markHeight, 2);
+var johnBMI = johnMass / Math.pow(johnHeight, 2);
+
+var markHigherThanJohn = markBMI > johnBMI;
+
+console.log(`Is Mark's BMI higher than John's?
+${markHigherThanJohn}. It is ${markBMI} while
+John's is ${johnBMI}.`);
