e7c0e7d9683ecf39fb210861edb1582d0ecf5882
Joanna <jmgsese@gmail.com>
2018-12-03 21:24:11 -0600

    Code Challenge #2

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -78,3 +78,39 @@ switch (job) {
   default:
     console.log(`${firstName} does something else.`)
 }
+
+// * CODING CHALLENGE 2
+// */
+//
+// /*
+// // John and Mike both play basketball in different teams.
+// In the latest 3 games, John's team scored 89, 120 and 103
+// points, while Mike's team scored 116, 94 and 123 points.
+// //
+// // 1. Calculate the average score for each team
+// // 2. Decide which teams wins in average (highest average
+// score), and print the winner to the console. Also include the average score in the output.
+// // 3. Then change the scores to show different winners.
+// Don't forget to take into account there might be a draw (the same average score)
+// //
+// // 4. EXTRA: Mary also plays basketball, and her team scored
+// 97, 134 and 105 points. Like before, log the average winner
+ // to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+// // 5. Like before, change the scores to generate different
+// winners, keeping in mind there might be draws.
+//
+// GOOD LUCK 😀
+
+var teamJohn = Math.floor((123+120+103)/3)
+var teamMike = Math.floor((116+94+123)/3)
+var teamMary = Math.floor((200+134+105)/3)
+
+if (teamJohn > teamMike && teamJohn > teamMary){
+  console.log(`Team John wins with ${teamJohn} points!`)
+} else if (teamMike > teamJohn && teamMike > teamMary){
+  console.log(`Team Mike wins with ${teamMike} points!`)
+} else if (teamMary > teamJohn && teamMary > teamMike){
+  console.log(`Team Mary wins with ${teamMary} points!`)
+} else {
+  console.log('It\'s a tie!')
+}
