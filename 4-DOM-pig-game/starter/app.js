8f5037f224ff8b6ceab526dd7ae0af2fd30e3a10
Joanna <jmgsese@gmail.com>
2018-12-07 15:04:51 -0600

    Add dice roll functionality to Pig Game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -12,3 +12,24 @@ GAME RULES:
 let scores  = [0,0];
 let roundScore = 0;
 let activePlayer = 0;
+
+// lets us select elements by css element
+// document.querySelector(`#current-${activePlayer}`).textContent = dice;
+// innerHTML must be a string
+// document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`;
+
+// hide dice
+document.querySelector('.dice').style.display = 'none';
+
+// select dice roll event
+btn = () => {
+  // 1. Generate random number
+  let dice = Math.floor(Math.random() * 6) + 1;
+  // 2. Display result
+  let diceDOM = document.querySelector('.dice')
+  diceDOM.style.display = 'block';
+  diceDOM.src = `dice-${dice}.png`;
+  // 3. Update the round score IF result !== 1
+}
+
+document.querySelector('.btn-roll').addEventListener('click', btn); // pass action and callback function
