7e60773903065bd8fef16145ceadfeec900b2234
Joanna <jmgsese@gmail.com>
2018-12-17 03:05:04 -0600

    Build out winner features

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -48,9 +48,17 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
   // Update UI
   document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
   // Check if player won the game
+    if (scores[activePlayer] >= 10){
+      document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!'
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+      document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active')
 
-  // Next player
-  nextPlayer();
+    } else {
+      // Next player
+      nextPlayer();
+    }
+
 });
 
 function nextPlayer(){
