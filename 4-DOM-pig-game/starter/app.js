62af55c3619196742da3a2a5fd6a280474becf57
Joanna Sese <joannasese@Joannas-MacBook.local>
2018-12-17 17:11:43 -0600

    Reset Player 1 and Player 2 when clicking 'New Game'

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,16 +8,9 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
-
-// initially hide dice
-document.querySelector('.dice').style.display = 'none';
-// set initial values to 0
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 // select dice roll event
 btn = () => {
@@ -77,12 +70,20 @@ function nextPlayer(){
 }
 
 // New game
-document.querySelector('.btn-new').addEventListener('click', function(){
-  init();
-})
+document.querySelector('.btn-new').addEventListener('click', init)
 
 function init(){
   scores = [0,0];
   activePlayer = 0;
   roundScore = 0;
-}
+
+  // initially hide dice
+  document.querySelector('.dice').style.display = 'none';
+  // set initial values to 0
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+};
