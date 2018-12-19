afea8e0d7da91bb438c4be8cd1fd4526f1f11e8d
Joanna Sese <joannasese@Joannas-MacBook.local>
2018-12-19 13:22:50 -0600

    Review Object.create

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,49 +1,68 @@
-// Inheritance in JS: Prototypes and prototype chains
-
-// Every object has a prototype property, which makes inheritance possible in js
-// The prototype property of an object is where we put methods and properties that we want other objects to inherit
-// The Constructor's prototype property is NOT the prototype of the Constructor,
-// it's the prototype of all instances that are created through it
-// When a certain method (or property) is called, the search starts in the object itself,
-// and if it cannot be found, the search moves to the object's prototype.
-// The search continues until the method is found.
-// This is the protoype chain.
-
-// Function constructor
-
-// var joanna = {
-//   name: 'Joanna';
-//   yearOfBirth: 1986;
-//   job: 'Panda';
+// // LECTURE 61: Inheritance in JS: Prototypes and prototype chains
+//
+// // Every object has a prototype property, which makes inheritance possible in js
+// // The prototype property of an object is where we put methods and properties that we want other objects to inherit
+// // The Constructor's prototype property is NOT the prototype of the Constructor,
+// // it's the prototype of all instances that are created through it
+// // When a certain method (or property) is called, the search starts in the object itself,
+// // and if it cannot be found, the search moves to the object's prototype.
+// // The search continues until the method is found.
+// // This is the protoype chain.
+//
+// // Function constructor
+//
+// // var joanna = {
+// //   name: 'Joanna';
+// //   yearOfBirth: 1986;
+// //   job: 'Panda';
+// // };
+//
+// // function constructors being with capital letter
+// var Person = function(name, yearOfBirth, job){
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.job = job;
+//   // this.calculateAge = function() {
+//   //   console.log(2018-this.yearOfBirth);
+//   // }
 // };
+//
+// // prototype property allows for
+// // method is not in constructor but can still be used
+// // bc it's in the prototype property of the function constructor
+// Person.prototype.calculateAge =
+// function() {
+//   console.log(2018-this.yearOfBirth);
+// }
+// Person.prototype.lastName = 'Jambalaya';
+// // use function constructor to create object
+// // called 'instantiation'
+// // 1. first, new operator creates an empty object
+// // 2. function is called
+// var joanna = new Person('Joanna', 1986, 'panda');
+// var lucy = new Person('Lucy', 1678, 'dead');
+// var kim = new Person('Kim', 1243, 'ancient');
+//
+// joanna.calculateAge();
+// lucy.calculateAge();
+// kim.calculateAge();
+// console.log(joanna.lastName);
 
-// function constructors being with capital letter
-var Person = function(name, yearOfBirth, job){
-  this.name = name;
-  this.yearOfBirth = yearOfBirth;
-  this.job = job;
-  // this.calculateAge = function() {
-  //   console.log(2018-this.yearOfBirth);
-  // }
+// LECTURE 63: Creating Objects with object.create
+var personProto = {
+  calculateAge: function() {
+    console.log(2018-this.yearOfBirth)
+  }
 };
 
-// prototype property allows for
-// method is not in constructor but can still be used
-// bc it's in the prototype property of the function constructor
-Person.prototype.calculateAge =
-function() {
-  console.log(2018-this.yearOfBirth);
-}
-Person.prototype.lastName = 'Jambalaya';
-// use function constructor to create object
-// called 'instantiation'
-// 1. first, new operator creates an empty object
-// 2. function is called
-var joanna = new Person('Joanna', 1986, 'panda');
-var lucy = new Person('Lucy', 1678, 'dead');
-var kim = new Person('Kim', 1243, 'ancient');
+var joanna = Object.create(personProto);
+joanna.name = 'Joanna';
+joanna.yearOfBirth = 1986;
+joanna.job = 'panda';
 
-joanna.calculateAge();
-lucy.calculateAge();
-kim.calculateAge();
-console.log(joanna.lastName);
+var jim = Object.create(personProto,
+{
+  name: {value: 'Jim'},
+  yearOfBirth: {value: 1374},
+  job: {value: 'designer'}
+});
