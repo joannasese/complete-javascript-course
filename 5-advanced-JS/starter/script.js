b167b8208cfb09228cd6fcd94850f43c92be4932
Joanna Sese <jmgsese@gmail.com>
2018-12-20 15:20:13 -0600

    Complete code challenge part 2

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -292,6 +292,18 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
 // var Question = function(){
 //   var questions = [
 //     'What color are piglets?',
@@ -345,6 +357,64 @@ c) correct answer (I would use a number for this)
 // Question();
 
 
+// (function (){ //iife
+// // function constructor
+// function Question(question, answers, correct){
+//   this.question = question;
+//   this.answers = answers;
+//   this.correct = correct;
+// }
+//
+// var answer;
+//
+// Question.prototype.displayQuestion =
+//   function(){
+//     console.log(this.question);
+//
+//     for (var i = 0; i < this.answers.length; i++){
+//       console.log(`${i}: ${this.answers[i]}`)
+//     }
+//   }
+//
+// Question.prototype.checkAnswer =
+//   function(el){
+//     if (el === this.correct){
+//       console.log('Yea you right.');
+//     } else {
+//       console.log('Sorry try again.');
+//     }
+//   }
+//
+// var q1 = new Question(
+//   'What color are piglets?',
+//   ['Pink', 'Blue', 'Chartreuse'],
+//   0
+// );
+//
+// var q2 = new Question(
+//   'Is water wet?',
+//   ['Yes', 'No', 'Maybe'],
+//   0
+// );
+//
+// var q3 = new Question(
+//   'Why is there glitter everywhere?',
+//   ['Because why not?', 'I don\'t know what you\'re talking about.', 'I don\'t know but I love it.'],
+//   2
+// );
+//
+// var questions = [q1, q2, q3];
+// var randomQuestion = (Math.floor(Math.random() * questions.length));
+//
+//
+// questions[randomQuestion].displayQuestion();
+//
+// var answer = parseInt(prompt('Type in your answer.'));
+//
+// questions[randomQuestion].checkAnswer(answer);
+// })();
+
+// EXPERT LEVEL
 (function (){ //iife
 // function constructor
 function Question(question, answers, correct){
@@ -352,8 +422,6 @@ function Question(question, answers, correct){
   this.answers = answers;
   this.correct = correct;
 }
-
-var answer;
 
 Question.prototype.displayQuestion =
   function(){
@@ -365,13 +433,23 @@ Question.prototype.displayQuestion =
   }
 
 Question.prototype.checkAnswer =
-  function(el){
+  function(el, callback){
+    var sc;
     if (el === this.correct){
-      console.log('Yea you right.')
+      console.log('Yea you right.');
+      sc = callback(true);
     } else {
-      console.log('Sorry try again.')
+      console.log('Sorry try again.');
+      sc = callback(false);
     }
+    this.displayScore(sc);
   }
+
+  Question.prototype.displayScore =
+    function(score){
+      console.log(`Your score is ${score}`);
+      console.log('--------')
+    }
 
 var q1 = new Question(
   'What color are piglets?',
@@ -392,12 +470,29 @@ var q3 = new Question(
 );
 
 var questions = [q1, q2, q3];
-var randomQuestion = (Math.floor(Math.random() * questions.length));
 
+function playerScore(){
+  var score = 0;
+  return function(correct){
+    if (correct){
+      score++;
+    }
+    return score;
+  }
+}
 
-questions[randomQuestion].displayQuestion();
+var keepScore = playerScore();
 
-var answer = parseInt(prompt('Type in your answer.'));
+function nextQuestion(){
+  var randomQuestion = (Math.floor(Math.random() * questions.length));
+  questions[randomQuestion].displayQuestion();
+  var answer = prompt('Type in your answer.');
+  if (answer !== 'exit'){
+    questions[randomQuestion].checkAnswer(parseInt(answer), keepScore);
+    nextQuestion();
+  }
 
-questions[randomQuestion].checkAnswer(answer);
+}
+
+nextQuestion();
 })();
