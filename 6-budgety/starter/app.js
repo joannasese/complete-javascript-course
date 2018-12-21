217d47e8ce59c91f63e04f48c2e79cccbde4477a
Joanna Sese <jmgsese@gmail.com>
2018-12-21 16:17:59 -0600

    Create setupEventListeners function and init function

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,13 +1,11 @@
 // iife allows data privacy
 // BUDGET CONTROLLER
 var budgetController = (function() {
-  console.log('budget controller')
+  
 }());
 
 // UI CONTROLLER
 var uiController = (function() {
-  console.log('UI controller')
-
   var DOMstrings = {
     inputType: '.add__type',
     inputDescription: '.add__description',
@@ -32,24 +30,38 @@ var uiController = (function() {
 // GLOBAL APP CONTROLLER
 // pass controllers into global app controller
 var controller = (function(budgetCtrl, uiCtrl) {
-  var DOM = uiCtrl.getDOMstrings(); // retrieves DOMstrings from UI Controller
+  // pass other two modules into this controller
+  var setupEventListeners = function(){
+    // not an iife and will need to be called. see init function
+    var DOM = uiCtrl.getDOMstrings(); // retrieves DOMstrings from UI Controller
+
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+    // allows input data submission with 'enter'
+    document.addEventListener('keypress', function(event){
+      var key = event.which || event.keyCode;
+      if (key === 13){
+        ctrlAddItem();
+      }
+    });
+  };
+
   var ctrlAddItem = function(){
     // 1. Get the field input data
     var input = uiCtrl.getInput();
-    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-  }
-  // pass other two modules into this controller
-  document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-  // allows input data submission with 'enter'
-  document.addEventListener('keypress', function(event){
-    var key = event.which || event.keyCode;
-    if (key === 13){
-      ctrlAddItem();
+  };
+
+  return {
+    // make function public by returning
+    init: function(){
+      console.log('Application has started');
+      setupEventListeners(); // originally in an iife
     }
-  });
+  }
 
 })(budgetController, uiController);
+
+controller.init(); // without this line of code, nothing will happen
